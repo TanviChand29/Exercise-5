0bf30e2b27036d3fa7e31d7d75f6c5bfed7db077
hokkitokki <147754469+hokkitokki@users.noreply.github.com>
2023-11-14 15:54:31 -0500

    Update spotify.js

---
+++ spotify.js
@@ -0,0 +1,3 @@
+curl -X POST "https://accounts.spotify.com/api/token" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -d "grant_type=client_credentials&client_id=376dd56df7ba469eb5ba2ae11b475ccb&client_secret=5da102b1cf0f4b91adca550c96981af0"
