0964df69d8e9fe8101742552ae5627c4ff7aadc6
hokkitokki <147754469+hokkitokki@users.noreply.github.com>
2023-12-02 16:47:22 -0500

    .

--- spotify.js
+++ spotify.js
@@ -1,8 +1,8 @@
 window.onSpotifyIframeApiReady = (IFrameAPI) => {
      let element = document.getElementById('embed-iframe');
      let options = {
-          width: 400,
-          height: 400,
+          width: 1000,
+          height: 500,
          uri: 'spotify:playlist:0HsQ2Lh6Fl7SFr18hAfQmH' 
        };
      let callback = (EmbedController) => {};
