ec6c364c6467ba0d0ba4319bc11a94d5b7ed60a1
hokkitokki <147754469+hokkitokki@users.noreply.github.com>
2023-12-02 14:29:02 -0500

    WE DID IT
    UPDATE

--- spotify.js
+++ spotify.js
@@ -1,3 +1,14 @@
-curl -X POST "https://accounts.spotify.com/api/token" \
-     -H "Content-Type: application/x-www-form-urlencoded" \
-     -d "grant_type=client_credentials&client_id=376dd56df7ba469eb5ba2ae11b475ccb&client_secret=5da102b1cf0f4b91adca550c96981af0"
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+     let element = document.getElementById('embed-iframe');
+     let options = {
+          width: 400,
+          height: 400,
+         uri: 'spotify:playlist:0HsQ2Lh6Fl7SFr18hAfQmH' 
+       };
+     let callback = (EmbedController) => {};
+     IFrameAPI.createController(element, options, callback);
+   };
+
+   EmbedController.loadUri('spotify:playlist:0HsQ2Lh6Fl7SFr18hAfQmH');
+
+
